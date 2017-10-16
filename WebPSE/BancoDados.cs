307f55503d11def8b3bc6b5385e8cdcfa2a77859
hgcgomes <hgcgomes@users.noreply.github.com>
2017-10-16 20:35:25 -0200

    Aula de PSE

---
+++ WebPSE/BancoDados.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace WebPSE
+{
+    public class BancoDados : DbContext
+    {
+        public BancoDados()
+            : base("DefaultConnection")
+        {
+            
+        }
+
+        public System.Data.Entity.DbSet<WebPSE.Models.Pessoa> Pessoas { get; set; }
+    }
+}
---
+++ WebPSE/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebPSE.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
