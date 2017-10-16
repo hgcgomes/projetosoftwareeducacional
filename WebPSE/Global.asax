50898ba9425787e4dcd78a517d9801cb50409a26
hgcgomes <hgcgomes@users.noreply.github.com>
2017-10-16 20:15:00 -0200

    Add project files.

---
+++ WebPSE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPSE.MvcApplication" Language="C#" %>
---
+++ WebPSE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPSE.Startup))]
+namespace WebPSE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
